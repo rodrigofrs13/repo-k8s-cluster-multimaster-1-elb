cfadeaddca2826d096589f247aa5ba9bbf72ec5b
Rodrigo Fernandes <rodrigofrs13@gmail.com>
2021-06-14 20:44:19 -0300

    update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,3 +3,13 @@ provider "aws" {
   shared_credentials_file = "C:/Users/roee/.aws/credentials"
   profile                 = "default"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "remote-terraform-tfstate"
+    key            = "descomplicandokubernetes/terraform.state"
+    region         = "us-east-1" //não pode ser variavel
+    profile        = "default"
+    encrypt        = true
+  }
+}
